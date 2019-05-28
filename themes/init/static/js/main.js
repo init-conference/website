8dc1e4ba828a4a23ed46114559e881b48faffa35
Goran Jakovljevic <goranefbl@gmail.com>
2019-05-28 14:28:18 +0200

    Sticky nav

--- themes/init/static/js/main.js
+++ themes/init/static/js/main.js
@@ -1,28 +1,42 @@
 // Main JavaScript file
-(function() {
-	scrollTo();
-})();
+(function () {
+  // smooth scroll
+  scrollTo();
+
+  // Sticky menu
+  const section = document.getElementById('about');
+  const nav = document.getElementById('js--sticky-nav');
+  window.addEventListener('scroll', (event) => {
+    const fromTop = window.scrollY;
+    if (section.offsetTop <= fromTop) {
+      nav.classList.add('sticky');
+    } else {
+      nav.classList.remove('sticky');
+    }
+  });
+}());
 
 function scrollTo() {
-	const links = document.querySelectorAll('.scrollTo');
-	links.forEach(each => (each.onclick = scrollAnchors));
+  const links = document.querySelectorAll('.scrollTo');
+  links.forEach(each => (each.onclick = scrollAnchors));
 }
 
 function scrollAnchors(e, respond = null) {
-	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-	e.preventDefault();
-	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-	const targetAnchor = document.querySelector(targetID);
-	if (!targetAnchor) return;
-	const originalTop = distanceToTop(targetAnchor);
-	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-	const checkIfDone = setInterval(function() {
-		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-		if (distanceToTop(targetAnchor) === 0 || atBottom) {
-			targetAnchor.tabIndex = '-1';
-			targetAnchor.focus();
-			window.history.pushState('', '', targetID);
-			clearInterval(checkIfDone);
-		}
-	}, 100);
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  e.preventDefault();
+  const targetID = respond ? respond.getAttribute('href') : this.getAttribute('href');
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+
+  window.scrollBy({ top: originalTop - 150, left: 0, behavior: 'smooth' });
+  const checkIfDone = setInterval(() => {
+    const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = '-1';
+      targetAnchor.focus();
+      window.history.pushState('', '', targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
 }
